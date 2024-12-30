f81f6f7f0ddd226aaffd91f3c3a0f85ae4f1238f
CaptainSkyFish <ajaystd9756@gmail.com>
2024-12-30 22:04:45 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom"
+import Signup from "./pages/SignUp"
+import Signin from "./pages/SignIn"
+import Blog from "./pages/Blog"
+import Blogs from "./pages/Blogs"
+import Background from "./UI/Background"
+import "./App.css"
+import { useEffect, useState } from "react"
+
+function AppWrapper() {
+  const location = useLocation()
+  const [showBackground, setShowBackground] = useState(false)
+
+  useEffect(() => {
+    // Specify paths where the background should be hidden
+    const hiddenBackgroundRoutes = ["/signup", "/signin"]
+    setShowBackground(!hiddenBackgroundRoutes.includes(location.pathname))
+  }, [location])
+
+  return (
+    <div>
+        {showBackground && <Background />}
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppWrapper />
+    </BrowserRouter>
+  )
+}
---
+++ frontend/src/UI/Background.tsx
@@ -0,0 +1,31 @@
+export default function Background() {  
+    return (
+      <div className="top-bg">
+        <div className="background">
+          <div className="circle one"></div>
+          <div className="circle two"></div>
+  
+          {/* Grainy overlay */}
+          <svg className="grain">
+            <defs>
+              <filter id="grainy">
+                <feTurbulence
+                  type="fractalNoise"
+                  baseFrequency="0.8"
+                  numOctaves="3"
+                  stitchTiles="stitch"
+                />
+                <feColorMatrix type="saturate" values="0" />
+              </filter>
+            </defs>
+            <rect
+              width="100%"
+              height="100%"
+              fill="transparent"
+              filter="url(#grainy)"
+            />
+          </svg>
+        </div>
+      </div>
+    )
+  }
